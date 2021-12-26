c3eaaf71a9922a6e4fb6688efd4983f300f86c71
jdamp-wave <jdamp@wavecable.com>
2021-12-25 19:03:12 -0800

    one more attempt on status

--- main/gen_gdl90.go
+++ main/gen_gdl90.go
@@ -849,7 +849,7 @@ func stratusInfoSender() {
 	for {
 		<-timer.C
 		if globalSettings.Stratus_Enabled {
-			sendStratus(makeStratusStatus(), time.Second, 1)
+			sendGDL90(makeStratusStatus(), time.Second, 1)
 		}
 	}
 }
